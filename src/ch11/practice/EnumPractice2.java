6d58abe296251ede76f99792db275d87c90d1624
JanuaryKim <unool6976@gmail.com>
2022-07-15 22:47:08 +0900

    enum, generic, wildCard 연습

---
+++ src/ch11/practice/EnumPractice2.java
@@ -0,0 +1,43 @@
+package ch11.practice;
+
+enum Transportation{
+
+    BUS(1000) {
+        @Override
+        int calcuCost(int distance) {
+
+
+            return distance * basicCost;
+        }
+    },
+    TAXI(2000){
+        @Override
+        int calcuCost(int distance) {
+            return distance * basicCost;
+        }
+    },
+    AIRPLAINE(40000){
+        @Override
+        int calcuCost(int distance) {
+            return (distance * basicCost) * 2;
+        }
+    };
+
+    protected final int basicCost; //protected로 해야 각 상수에서 접근 가능
+
+
+    Transportation(int basicCost) {
+        this.basicCost = basicCost;
+    }
+
+    abstract int calcuCost(int basicCost);
+
+}
+
+public class EnumPractice2 {
+    public static void main(String[] args) {
+        Transportation transportation = Transportation.AIRPLAINE;
+
+        System.out.println(transportation.calcuCost(2));
+    }
+}
